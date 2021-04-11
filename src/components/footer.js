66b4ed3f1715ca60d9b92f752921c6e59bef0157
Hirochika Yuda <hyuda@outlook.jp>
2021-04-11 15:45:55 +0900

    Fix gatsby-plugin-ackee-tracker version mismatch

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,8 +11,12 @@ export default ({className, style}) => {
     return (
       <div className={setClass} style={style}>
         <div className="md:flex-grow">
-          <a href="https://kuropen.org/" className="underline" target="_blank" rel="noopener noreferrer">Copyright (C) Kuropen</a>.
-          <a href="https://penguinone.kuropen.org/about#anchor-privacy" className="underline" target="_blank" rel="noopener noreferrer">プライバシーポリシー</a>
+          <div className="md:inline">
+            <a href="https://kuropen.org/" className="underline" target="_blank" rel="noopener noreferrer">Copyright (C) Kuropen</a>.
+          </div>
+          <div className="md:inline">
+            <a href="https://penguinone.kuropen.org/about#anchor-privacy" className="underline" target="_blank" rel="noopener noreferrer">プライバシーポリシー</a>
+          </div>
         </div>
         <div className="md:text-right md:justify-end">
           <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="クリエイティブ・コモンズ・ライセンス" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a>
